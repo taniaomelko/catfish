8f2d175154d7443198121441a7240a1c96717fe9
Tania Omelko <tanyaomelko39@gmail.com>
2020-11-25 23:11:36 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const catfish = document.querySelector('.catfish');
+const closeCatfish = document.querySelector('.catfish__close');
+
+closeCatfish.addEventListener('click', () => {
+  catfish.classList.add('catfish--is-closed');
+});
